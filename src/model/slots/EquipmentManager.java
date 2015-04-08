052e575780307c96b716cb24f863a60bdceea6b7
jackstine <ecstaticjack@gmail.com>
2015-04-08 18:55:05 -0400

    EquipmentManager

--- src/model/slots/EquipmentManager.java
+++ src/model/slots/EquipmentManager.java
@@ -18,86 +18,86 @@ public class EquipmentManager {
 	private EquipmentSlot<Boots> bootsSlot;
 	private EquipmentSlot<Gloves> glovesSlot;
 	private EquipmentSlot<Projectile> projectileSlot;
-	
-	public EquipmentManager(){ //TODO (AvatarTypes)
-		this.setSlots(); //TODO (WeaponType)
+
+	public EquipmentManager() { // TODO (AvatarTypes)
+		this.setSlots(); // TODO (WeaponType)
 	}
-	
-	private void setSlots(){  //TODO (Weapon)
+
+	private void setSlots() { // TODO (Weapon)
 		this.helmetSlot = new EquipmentSlot<Helmet>();
-		this.chestPieceSlot = new EquipmentSlot<ChestPiece>();	
+		this.chestPieceSlot = new EquipmentSlot<ChestPiece>();
 		this.leggingsSlot = new EquipmentSlot<Leggings>();
-		this.weaponSlot = new EquipmentSlot<Weapon>(); 
+		this.weaponSlot = new EquipmentSlot<Weapon>();
 		this.shieldSlot = new EquipmentSlot<Shield>();
 		this.bootsSlot = new EquipmentSlot<Boots>();
 		this.glovesSlot = new EquipmentSlot<Gloves>();
 		this.projectileSlot = new EquipmentSlot<Projectile>();
 	}
-	
-	/************************* 	UNEQUIP************************************/
-	public Projectile unequipProjectile(){
+
+	/************************* UNEQUIP ************************************/
+	public Projectile unequipProjectile() {
 		return this.projectileSlot.unequip();
 	}
-	
-	public Gloves unequipGloves(){
+
+	public Gloves unequipGloves() {
 		return this.glovesSlot.unequip();
 	}
-	
-	public Boots unequipBoots(){
+
+	public Boots unequipBoots() {
 		return this.bootsSlot.unequip();
 	}
-	
-	public Shield unequipShield(){
+
+	public Shield unequipShield() {
 		return this.shieldSlot.unequip();
 	}
-	
-	public Weapon unequipWeapon(){
+
+	public Weapon unequipWeapon() {
 		return this.weaponSlot.unequip();
 	}
-	
-	public Leggings unequipLeggings(){
+
+	public Leggings unequipLeggings() {
 		return this.leggingsSlot.unequip();
 	}
-	
-	public ChestPiece unequipChestPiece(){
+
+	public ChestPiece unequipChestPiece() {
 		return this.chestPieceSlot.unequip();
 	}
-	
-	public Helmet unequipHelmet(){
+
+	public Helmet unequipHelmet() {
 		return this.helmetSlot.unequip();
 	}
 
-	/*************************EQUIP**************************/
-	
-	public void equip(Projectile item){
+	/************************* EQUIP **************************/
+
+	public void equip(Projectile item) {
 		this.projectileSlot.equip(item);
 	}
-	
-	public void equip(Gloves item){
+
+	public void equip(Gloves item) {
 		this.glovesSlot.equip(item);
 	}
-	
-	public void equip(Boots item){
+
+	public void equip(Boots item) {
 		this.bootsSlot.equip(item);
 	}
-	
-	public void equip(Shield item){
+
+	public void equip(Shield item) {
 		this.shieldSlot.equip(item);
 	}
-	
-	public void equip(Weapon item){  //TODO    special case
+
+	public void equip(Weapon item) { // TODO special case for all types
 		this.weaponSlot.equip(item);
 	}
-	
-	public void equip(Leggings item){
+
+	public void equip(Leggings item) {
 		this.leggingsSlot.equip(item);
 	}
-	
-	public void equip(ChestPiece item){
+
+	public void equip(ChestPiece item) {
 		this.chestPieceSlot.equip(item);
 	}
-	
-	public void equip(Helmet item){
+
+	public void equip(Helmet item) {
 		this.helmetSlot.equip(item);
 	}
 }
---
+++ src/model/slots/InventoryEquipment.java
@@ -0,0 +1,106 @@
+package model.slots;
+
+import utilities.Point;
+import model.item.Boots;
+import model.item.ChestPiece;
+import model.item.Equipable;
+import model.item.Gloves;
+import model.item.Helmet;
+import model.item.Leggings;
+import model.item.Projectile;
+import model.item.Shield;
+import model.item.TakeableItem;
+import model.item.Weapon;
+
+public class InventoryEquipment {
+	private EquipmentManager equipment;
+	private Inventory inventory;
+
+	public InventoryEquipment() { // TODO
+		this.equipment = new EquipmentManager();
+		this.inventory = new Inventory();
+	}
+
+	/************ INVENTORY ********************/
+	public boolean insert(TakeableItem item) {
+		return this.inventory.insert(item);
+	}
+
+	public TakeableItem remove(TakeableItem item) throws Exception {
+		return this.inventory.remove(item);
+	}
+
+	public TakeableItem remove(int x, int y) {
+		return this.inventory.remove(x, y);
+	}
+
+	public TakeableItem remove(Point point) {
+		return this.inventory.remove(point);
+	}
+
+	public TakeableItem[][] getInventoryItems() {
+		return this.inventory.getItems();
+	}
+
+	public TakeableItem getInventoryItem(int x, int y) {
+		return this.inventory.get(x, y);
+	}
+
+	public TakeableItem getInventoryItem(Point point) {
+		return this.inventory.get(point);
+	}
+
+	public boolean inventoryHas(int x, int y) {
+		return this.inventory.slotHas(x, y);
+	}
+
+	public boolean inventoryHas(Point point) {
+		return this.inventory.slotHas(point);
+	}
+
+	public boolean inventoryHasItem(TakeableItem item, int x, int y) {
+		return this.inventory.slotHasItem(item, x, y);
+	}
+
+	public boolean inventoryHasItem(TakeableItem item, Point point) {
+		return this.inventory.slotHasItem(item, point);
+	}
+
+	/***************************** EQUIPMENT ********************************/
+	public void equip(Equipable item) {
+//		this.equipment.equip(item);
+		//TODO
+	}
+	
+	public Projectile unequipProjectile(){
+		return this.equipment.unequipProjectile();
+	}
+	
+	public Gloves unequipGloves(){
+		return this.equipment.unequipGloves();
+	}
+	
+	public Boots unequipBoots(){
+		return this.equipment.unequipBoots();
+	}
+	
+	public Shield unequipShield(){
+		return this.equipment.unequipShield();
+	}
+	
+	public Weapon unequipWeapon(){
+		return this.equipment.unequipWeapon();
+	}
+	
+	public Leggings unequipLeggings(){
+		return this.equipment.unequipLeggings();
+	}
+	
+	public ChestPiece unequipChestPiece(){
+		return this.equipment.unequipChestPiece();
+	}
+	
+	public Helmet unequipHelmet(){
+		return this.equipment.unequipHelmet();
+	}
+}
