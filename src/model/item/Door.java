fe5e45ff8c78640f81cc31b05c6c2094fd0bc84f
Joseph Liccini <joseph.liccini@ufl.edu>
2015-04-08 20:14:18 -0400

    Door class.

---
+++ src/model/item/Door.java
@@ -0,0 +1,33 @@
+package model.item;
+
+import model.entity.Entity;
+
+public class Door extends Item {
+
+	private TakeableItem requirement; // required to open the Door
+	private boolean open = false;
+	
+	@Override
+	public void touch(Entity entity) {
+		// Should I do something different on touch()? -- Joe
+		use(entity);
+	}
+
+	@Override
+	public void use(Entity entity) {
+		/* if (entity.containsItem(requirement)) {
+			open = true;
+		} */
+	}
+
+	@Override
+	public String getInfo() {
+		return "This door is: " + open;
+	}
+	
+	@Override
+	public boolean isBlocking() {
+		return !open;
+	}
+
+}
--- src/model/item/Item.java
+++ src/model/item/Item.java
@@ -10,5 +10,11 @@ public abstract class Item {
 	public abstract void touch(Entity entity);
 	public abstract void use(Entity entity);
 	public abstract String getInfo();
+	
+	// Referring to Blocking of Movement.
+	// Missing in Spec, but it is in the ItemBlocking UML Digram
+	public boolean isBlocking() {
+		return false;
+	}
 
 }
