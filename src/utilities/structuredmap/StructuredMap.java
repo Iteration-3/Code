3e5028ac1243fed5707ac3c20d627fb3f3ae527a
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2015-04-08 20:56:58 -0400

    Adding structuredmap scaffolding

---
+++ src/utilities/structuredmap/StructuredMap.java
@@ -0,0 +1,62 @@
+package utilities.structuredmap;
+
+public class StructuredMap {
+	  public StructuredMap() {
+	  }
+
+	  public void put(String key, Object value) {
+	  }
+
+	  public void put(String key, Boolean value) {
+	  }
+
+	  public void put(String key, Integer value) {
+	  }
+
+	  public void put(String key, Double value) {
+	  }
+
+	  public void put(String key, String value) {
+	  }
+
+	  public void put(String key, StructuredMap value) {
+	  }
+
+	  public void put(String key, int[] values) {
+	  }
+
+	  public void put(String key, StructuredMap[] values) {
+	  }
+
+	  public Boolean getBoolean(String key) {
+		  return null;
+	  }
+
+	  public Double getDouble(String key) {
+		  return null;
+	  }
+
+	  public Integer getInteger(String key) {
+		  return null;
+	  }
+
+	  public String getString(String key) {
+		  return null;
+	  }
+
+	  public StructuredMap getStructuredMap(String key) {
+		  return null;
+	  }
+
+	  public int[] getIntArray(String key) {
+		  return null;
+	  }
+
+	  public StructuredMap[] getStructuredMapArray(String key) {
+		  return null;
+	  }
+
+	  public Set<String> getKeys() {
+		  return null;
+	  }
+}
