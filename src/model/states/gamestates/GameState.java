ee030fa42f1ba5708024dc5f31026adbc5da37d1
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2015-04-08 20:00:29 -0400

    Adding state machine scaffolding

---
+++ src/model/states/gamestates/GameState.java
@@ -0,0 +1,17 @@
+package model.states.gamestates;
+
+import model.states.State;
+
+public class GameState extends State {
+	@Override
+	public void onEnter() {}
+	
+	@Override
+	public void onPause() {}
+	
+	@Override
+	public void onResume() {}
+	
+	@Override
+	public void onExit() {}
+}
