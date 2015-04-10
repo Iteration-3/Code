370f4b4b3cba18a39df3075b32caab21a0873e21
jeanralphaviles <jeanralph.aviles@gmail.com>
2015-04-10 16:40:22 -0400

    Projectile Manager and Projectiles

---
+++ src/projectile/ConicalProjectile.java
@@ -0,0 +1,46 @@
+package projectile;
+
+import utilities.Angle;
+import model.area.Location;
+import model.trigger.TimedTrigger;
+import model.trigger.Trigger;
+
+public class ConicalProjectile extends Projectile {
+
+	public ConicalProjectile() {
+		super();
+		this.setTrigger(new TimedTrigger(this.getTrigger().getArea(), this.getTrigger().getEvent(), 5));
+	}
+
+	public ConicalProjectile(Angle direction, Location location, double speed,
+			long timeout, Trigger trigger) {
+		super(direction, location, speed, timeout, trigger);
+	}
+
+	/* (non-Javadoc)
+	 * @see projectile.Projectile#advance()
+	 * This will create two linear triggers on the edges of the cone and move up by one
+	 */
+	@Override
+	public void advance() {
+		Angle leftAngle = this.getDirection().getLeft();
+		Angle rightAngle = this.getDirection().getRight();
+		Projectile leftProjectile = new Projectile(leftAngle, this
+				.getLocation().nextLocation(leftAngle), this.getSpeed(),
+				this.getTimeout(), this.getTrigger());
+		Projectile rightProjectile = new Projectile(rightAngle, this
+				.getLocation().nextLocation(rightAngle), this.getSpeed(),
+				this.getTimeout(), this.getTrigger());
+		ProjectileManager.addProjectile(leftProjectile);
+		ProjectileManager.addProjectile(rightProjectile);
+
+		this.setLocation(this.getLocation().nextLocation(this.getDirection()));
+		this.getTrigger().moveLocation(this.getLocation());
+	}
+
+	@Override
+	public boolean hasExpired() {
+		return getTrigger().hasExpired(); // TODO(jraviles) figure out collisions
+	}
+
+}
---
+++ src/projectile/Projectile.java
@@ -0,0 +1,98 @@
+package projectile;
+
+import model.area.Location;
+import model.area.RadialArea;
+import model.event.StatisticModifierEvent;
+import model.trigger.SingleUseTrigger;
+import model.trigger.Trigger;
+import model.trigger.TriggerManager;
+import statistics.EntityStatistics;
+import utilities.Angle;
+
+public class Projectile {
+	private Angle direction;
+	private Location location;
+	private double speed;
+	private long timeout;
+	private Trigger trigger;
+
+	public Projectile() {
+		this.direction = Angle.UP;
+		this.location = new Location();
+		this.speed = 1;
+		this.timeout = 1;
+		this.trigger = new SingleUseTrigger(new RadialArea(1, this.location),
+				new StatisticModifierEvent(new EntityStatistics(), 5));
+		TriggerManager.addNeutralTrigger(trigger);
+	}
+
+	public Projectile(Angle direction, Location location, double speed,
+			long timeout, Trigger trigger) {
+		this.direction = direction;
+		this.location = location;
+		this.speed = speed;
+		this.timeout = timeout;
+		this.trigger = trigger;
+		TriggerManager.addNeutralTrigger(trigger);
+	}
+
+	public void advance() {
+		if (!isTimedOut()) {
+			this.location = location.nextLocation(direction);
+			trigger.moveLocation(location);
+			timeOutProjectile();
+		}
+	}
+
+	public boolean hasExpired() {
+		return trigger.hasExpired(); // TODO(jraviles) expire on collision
+	}
+
+	private void timeOutProjectile() {
+		this.timeout = (long) (System.currentTimeMillis() + 1 / speed);
+	}
+
+	private boolean isTimedOut() {
+		return this.timeout > System.currentTimeMillis();
+	}
+
+	public Angle getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Angle direction) {
+		this.direction = direction;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public long getTimeout() {
+		return timeout;
+	}
+
+	public void setTimeout(long timeout) {
+		this.timeout = timeout;
+	}
+
+	public Trigger getTrigger() {
+		return trigger;
+	}
+
+	public void setTrigger(Trigger trigger) {
+		this.trigger = trigger;
+	}
+}
