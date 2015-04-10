67d61f6846294c9f55751daff5d9da2936abf1a0
Matthew <matthewbregg610@ufl.edu>
2015-04-09 20:32:54 -0400

    Added the inventory class to the item package

---
+++ src/model/item/Inventory.java
@@ -0,0 +1,42 @@
+package model.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import utilities.structuredmap.SavableLoadable;
+import utilities.structuredmap.StructuredMap;
+
+class Inventory implements SavableLoadable{
+	private ArrayList<TakeableItem> inventory_ = new ArrayList<TakeableItem>();
+	
+	public boolean addItem(TakeableItem item){
+		return inventory_.add(item);
+	}
+	public boolean removeItem(TakeableItem item){
+		return inventory_.remove(item);
+	}
+	
+	public boolean hasItem(TakeableItem item){
+		return inventory_.contains(item);
+	}
+	public List<TakeableItem> getItems(){
+		return inventory_;
+	}
+	/**
+	 * Unimplemented
+	 */
+	@Override
+	public StructuredMap getStructuredMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/**
+	 * unimplemented
+	 */
+	@Override
+	public void load(StructuredMap map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
