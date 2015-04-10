0e0ee2dd37852d60d4f27b0882f31914675a9adf
Matthew <matthewbregg610@ufl.edu>
2015-04-10 16:41:44 -0400

    Added savableloadable to maps

--- src/model/map/ItemMap.java
+++ src/model/map/ItemMap.java
@@ -1,5 +1,20 @@
 package model.map;
 
-public class ItemMap {
+import utilities.structuredmap.SavableLoadable;
+import utilities.structuredmap.StructuredMap;
+
+public class ItemMap implements SavableLoadable {
+
+	@Override
+	public StructuredMap getStructuredMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void load(StructuredMap map) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
