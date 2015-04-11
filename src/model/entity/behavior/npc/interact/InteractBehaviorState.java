be61f7b35aef4f2eba46d06e458c62fcaae39a09
jackstine <ecstaticjack@gmail.com>
2015-04-11 17:13:53 -0400

    delete something

--- src/model/entity/behavior/npc/interact/InteractBehaviorState.java
+++
@@ -1,9 +0,0 @@
-package model.entity.behavior.npc.interact;
-
-import model.entity.Entity;
-
-public interface InteractBehaviorState {
-	
-	public void interact(Entity entity);
-
-}
--- src/model/entity/behavior/npc/observe/ObserveBehaviorState.java
+++
@@ -1,5 +0,0 @@
-package model.entity.behavior.npc.observe;
-
-public interface ObserveBehaviorState {
-	public void observe();
-}
