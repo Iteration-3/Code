669822c2144870f4054a0581644e00a766e285b2
Matthew <matthewbregg610@ufl.edu>
2015-04-11 15:46:36 -0400

    Did the skillmanagers

---
+++ src/model/skillmanager/SkillManager.java
@@ -0,0 +1,41 @@
+package model.skillmanager;
+
+import utilities.structuredmap.SavableLoadable;
+
+public abstract class SkillManager implements SavableLoadable {
+	
+	private int attackSkill = 1;
+	private int bindWoundSkill = 1;
+	private int observeSkill = 1;
+	private int barterSkill = 1;
+	public int getBarterSkill(){
+		return barterSkill;
+	}
+	
+	
+	public int getObserveSkill(){
+		return observeSkill;
+	}
+	
+	public int getBindWoundsSkill(){
+		return bindWoundSkill;
+	}
+	
+	public int getAttackSkill(){
+		return attackSkill;
+	}
+	
+	public void incrementAttack(){
+		++attackSkill;
+	}
+	public void incrementBindWound(){
+		++bindWoundSkill;
+	}
+	public void incrementObserve(){
+		++observeSkill;
+	}
+	public void incrementBarter(){
+		++barterSkill;
+	}
+	
+}
---
+++ src/model/skillmanager/SneakSkillManager.java
@@ -0,0 +1,50 @@
+package model.skillmanager;
+
+import utilities.structuredmap.StructuredMap;
+
+public class SneakSkillManager extends SkillManager {
+
+	@Override
+	public StructuredMap getStructuredMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void load(StructuredMap map) {
+		// TODO Auto-generated method stub
+		
+	}
+	private int creepSkill = 1;
+	private int pickPicketSkill = 1;
+	private int trapRemoveSkill = 1;
+	private int rangedSkill = 1;
+	
+	public int getCreepskill(){
+		return creepSkill;
+	}
+	public int getPickPocketSkill(){
+		return pickPicketSkill;
+	}
+	public int trapRemoveSkill(){
+		return trapRemoveSkill;
+	}
+	public int rangedSkill(){
+		return rangedSkill;
+	}
+	
+	public void incrementRangedSkill(){
+		++rangedSkill;
+	}
+	public void incrementTrapRemoveSkill(){
+		++trapRemoveSkill;
+	}
+	public void incrementPickPocketSkill(){
+		++pickPicketSkill;
+	}
+	public void incrementCreepSkill(){
+		++creepSkill;
+	}
+	
+	
+}
