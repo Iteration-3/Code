99aafe385929bc77454076145c21919f239d7969
Joseph Liccini <joseph.liccini@ufl.edu>
2015-04-15 22:29:28 -0400

    Item on the Map!

--- src/view/map/GameItemView.java
+++ src/view/map/GameItemView.java
@@ -3,16 +3,49 @@ package view.map;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-import view.ItemView;
+import model.area.RealCoordinate;
+import view.item.ItemView;
 
+// Copied from GameTerrainView
 public class GameItemView implements GameView {
-	private Collection<ItemView> ItemViews = new ArrayList<ItemView>();
+	private Map<RealCoordinate, ItemView> itemViews = new HashMap<RealCoordinate, ItemView>();
+	private int screenWidth;
+	private int screenHeight;
+	private static final int NUM_TILES_Y = 18;  
+	private static final float TILE_DIMENSION_RATIO = (float) (Math.sqrt(3) / 2);
+
 	@Override
 	public void render(Graphics graphics, int width, int height) {
-		// TODO(mbregg)		
+		this.screenWidth = width;
+		this.screenHeight = height;
+		
+		for(Map.Entry<RealCoordinate, ItemView> entry: itemViews.entrySet()) {
+			ItemView itemView = entry.getValue();
+			RealCoordinate coordinate = entry.getKey();
+			int renderX = (int) (coordinate.getX() * itemWidth() * .75);
+			int renderY = (int) (coordinate.getY() * (coordinate.getX() % 2) * itemHeight() / 2);
+			itemView.render(graphics, new RealCoordinate(renderX, renderY), itemWidth() / 1.5f);
+		}
+		
 	}
-	public void addEntityView(ItemView itemView) {
-		ItemViews.add(itemView);
+
+	public void addItemView(ItemView itemView, RealCoordinate position) {
+		itemViews.put(position, itemView);
 	}
+
+	private float itemHeight() {
+		return (float) screenHeight / NUM_TILES_Y;
+	}
+	
+	private float itemWidth() {
+		return (itemHeight() / TILE_DIMENSION_RATIO);
+	}
+	
+	private float aspectRation() {
+		return (float) screenWidth / screenHeight;
+	}
+
 }
