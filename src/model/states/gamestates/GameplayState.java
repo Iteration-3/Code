9afa9464b87e3d02204ff35dc90f0435ddd0fdb8
josephliccini <joseph.liccini@ufl.edu>
2015-04-17 18:14:49 -0400

    Changed a color of a few things

--- src/model/states/gamestates/GameplayState.java
+++ src/model/states/gamestates/GameplayState.java
@@ -32,108 +32,96 @@ import controller.listener.Listener;
 import controller.listener.SingleUseListener;
 
 public class GameplayState extends GameState {
-	private GameplayController controller;
-	private GameplayLayout layout;
-	private GameTerrain gameMap;
-	private ItemEntityAssociation itemEntityAssociation;
+    private GameplayController controller;
+    private GameplayLayout layout;
+    private GameTerrain gameMap;
+    private ItemEntityAssociation itemEntityAssociation;
 
-	public GameplayState() {
-		layout = new GameplayLayout();
-		controller = new GameplayController();
-		gameMap = new GameTerrain();
-	}
+    public GameplayState() {
+        layout = new GameplayLayout();
+        controller = new GameplayController();
+        gameMap = new GameTerrain();
+    }
 
-	@Override
-	public void onEnter() {
-		super.onEnter();
-		addTilesTest();
-		addEntityTest();
-	}
+    @Override
+    public void onEnter() {
+        super.onEnter();
+        addTilesTest();
+        addEntityTest();
+    }
 
-	public void addEntityTest() {
-		TileCoordinate loc = new TileCoordinate(3, 3);
-		EntityView eView = new EntityView(new Color(200, 200, 0), Color.orange,
-				TileCoordinate.convertToRealCoordinate(loc));
-		Avatar avatar = new Smasher("Smasher", eView, loc);
+    public void addEntityTest() {
+        TileCoordinate loc = new TileCoordinate(3, 3);
+        EntityView eView = new EntityView(new Color(200, 200, 0), Color.orange,
+                TileCoordinate.convertToRealCoordinate(loc));
+        Avatar avatar = new Smasher("Smasher", eView, loc);
 
-		KeyPreferences preferences = new KeyPreferences();
-		super.setPrefrences(preferences);
+        KeyPreferences preferences = new KeyPreferences();
+        super.setPrefrences(preferences);
 
-		Listener escapeListener = new SingleUseListener(preferences.getPauseKey(), new GameActionStatePush(
-				getContext(), new PauseMenuState()));
-		escapeListener.addAsBinding(getLayout());
+        Listener escapeListener = new SingleUseListener(preferences.getPauseKey(), new GameActionStatePush(
+                getContext(), new PauseMenuState()));
+        escapeListener.addAsBinding(getLayout());
 
-		Listener inventoryListener = new SingleUseListener(preferences.getInventoryKey(), new GameActionStatePush(
-				getContext(), new InventoryMenuState()));
-		inventoryListener.addAsBinding(getLayout());
+        Listener inventoryListener = new SingleUseListener(preferences.getInventoryKey(), new GameActionStatePush(
+                getContext(), new InventoryMenuState()));
+        inventoryListener.addAsBinding(getLayout());
 
-		Listener skillsListener = new SingleUseListener(preferences.getSkillsKey(), new GameActionStatePush(
-				getContext(), new SkillsMenuState()));
-		skillsListener.addAsBinding(getLayout());
-		
-		this.itemEntityAssociation = new ItemEntityAssociation(avatar);
+        Listener skillsListener = new SingleUseListener(preferences.getSkillsKey(), new GameActionStatePush(
+                getContext(), new SkillsMenuState()));
+        skillsListener.addAsBinding(getLayout());
 
-		Collection<Listener> listeners = 
-				new EntityMovementAssocation(avatar, gameMap,
-						itemEntityAssociation.getItemMap()).getListeners(preferences);
-		for (Listener listener : listeners) {
-			listener.addAsBinding(getLayout());
-			controller.addEntityListener(listener);
-		}
+        Collection<Listener> listeners = new GameEntityAssocation(avatar, gameMap).getListeners(preferences);
+        for (Listener listener : listeners) {
+            listener.addAsBinding(getLayout());
+            controller.addEntityListener(listener);
+        }
 
-		EntityManager.getSingleton().setAvatar(avatar);
-		eView.registerWithGameMapView(layout.getGameEntityView(), TileCoordinate.convertToRealCoordinate(loc));
+        EntityManager.getSingleton().setAvatar(avatar);
+        eView.registerWithGameMapView(layout.getGameEntityView(), TileCoordinate.convertToRealCoordinate(loc));
 
-		
-		ItemView takeableItemView = new BasicItemView(new Color(100, 60, 100), Color.GREEN);
-		RealCoordinate takeableItemViewPosition = new RealCoordinate(5, 5);
-		takeableItemView.registerWithGameItemView(layout.getGameItemView(), takeableItemViewPosition);
-		itemEntityAssociation.addItem(new TakeableItem(takeableItemView), RealCoordinate.convertToTileCoordinate(takeableItemViewPosition));
+        this.itemEntityAssociation = new ItemEntityAssociation(avatar);
+        ItemView takeableItemView = new BasicItemView(new Color(100, 60, 100), Color.GREEN);
+        RealCoordinate takeableItemViewPosition = new RealCoordinate(5, 5);
+        takeableItemView.registerWithGameItemView(layout.getGameItemView(), takeableItemViewPosition);
+        itemEntityAssociation.addItem(new TakeableItem(takeableItemView), takeableItemViewPosition);
 
-		ItemView obstacleItemView = new BasicItemView(Color.GRAY, Color.BLACK);
-		RealCoordinate obstacleItemPosition = new RealCoordinate(9, 7);
-		obstacleItemView.registerWithGameItemView(layout.getGameItemView(), obstacleItemPosition);
-		itemEntityAssociation.addItem(new ObstacleItem(obstacleItemView), RealCoordinate.convertToTileCoordinate(obstacleItemPosition));
+        ItemView obstacleItemView = new BasicItemView(Color.GRAY, Color.BLACK);
+        RealCoordinate obstacleItemPosition = new RealCoordinate(9, 7);
+        obstacleItemView.registerWithGameItemView(layout.getGameItemView(), obstacleItemPosition);
+        itemEntityAssociation.addItem(new ObstacleItem(obstacleItemView), obstacleItemPosition);
 
-		ItemView oneshotItemView = new BasicItemView(Color.GRAY, Color.BLACK);
-		RealCoordinate oneshotItemPosition = new RealCoordinate(13, 9);
-		oneshotItemView.registerWithGameItemView(layout.getGameItemView(), oneshotItemPosition);
-		itemEntityAssociation.addItem(new OneShotItem(oneshotItemView, new EntityStatistics()), RealCoordinate.convertToTileCoordinate(oneshotItemPosition));
-	}
+        ItemView oneshotItemView = new BasicItemView(Color.GREEN, Color.YELLOW);
+        RealCoordinate oneshotItemPosition = new RealCoordinate(13, 9);
+        oneshotItemView.registerWithGameItemView(layout.getGameItemView(), oneshotItemPosition);
+        itemEntityAssociation.addItem(new OneShotItem(oneshotItemView, new EntityStatistics()), oneshotItemPosition);
+    }
 
-	public void addTilesTest() {
-		for (int x = 0; x < 100; ++x) {
-			for (int y = 0; y < 100; ++y) {// Hardcoded for as long as the area
-				// is
-				TileCoordinate p = new TileCoordinate(x, y);
-				if(x!=10 || y!=10){
-					TileView view = new BasicTileView(new Color(0, 200, 200), Color.WHITE);
-					view.registerWithGameMapView(layout.getGameTerrainView(), TileCoordinate.convertToRealCoordinate(p));
-					gameMap.add(new PassableTile(view), p);
-				}
-				else{
-					TileView view = new BasicTileView(new Color(200, 0, 200), Color.WHITE);
-					view.registerWithGameMapView(layout.getGameTerrainView(), TileCoordinate.convertToRealCoordinate(p));
-					gameMap.add(new ImpassableTile(view), p);
-				}
+    public void addTilesTest() {
+        for (int x = 0; x < 100; ++x) {
+            for (int y = 0; y < 100; ++y) {// Hardcoded for as long as the area
+                                           // is
+                TileView view = new BasicTileView(new Color(0, 200, 200), Color.WHITE);
+                TileCoordinate p = new TileCoordinate(x, y);
+                view.registerWithGameMapView(layout.getGameTerrainView(), TileCoordinate.convertToRealCoordinate(p));
+                gameMap.add(new PassableTile(view), p);
+            }
+        }
+    }
 
-			}
-		}
-	}
+    @Override
+    public GameplayLayout getLayout() {
+        return layout;
+    }
 
-	@Override
-	public GameplayLayout getLayout() {
-		return layout;
-	}
+    @Override
+    public GameplayController getController() {
+        return controller;
+    }
 
-	@Override
-	public GameplayController getController() {
-		return controller;
-	}
-
-	@Override
-	public void update() {
-		// TODO poll here.
-	}
+    @Override
+    public void update() {
+        // TODO poll here.
+    }
 
 }
