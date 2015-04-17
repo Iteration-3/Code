1c24c45925cae78eac6736bcfff3baed238af9f0
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2015-04-17 04:29:43 -0400

    Removing superfluous view from GameplayController

--- src/controller/GameplayController.java
+++ src/controller/GameplayController.java
@@ -1,23 +1,15 @@
 package controller;
 
 import controller.listener.Listener;
-import view.GameplayLayout;
 
 public class GameplayController {
 	private EntityController entityController;
-	private GameplayLayout layout;
 
 	public GameplayController() {
 		this.entityController = new EntityController();
-	}
-
-	public void setLayout(GameplayLayout layout) {
-		this.layout = layout;
 	}
 	
 	public void addEntityListener(Listener listener) {
 		entityController.addListener(listener);
 	}
-
-	
 }
