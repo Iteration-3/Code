801edac56cc7162a4c5b715763112b92e969f6d7
kkyrazis <kkyrazis@ufl.edu>
2015-04-17 19:07:53 -0400

    Fixed it so keybindings works

--- src/controller/GameplayController.java
+++ src/controller/GameplayController.java
@@ -3,17 +3,22 @@ package controller;
 import controller.listener.Listener;
 
 public class GameplayController extends Controller {
-	private EntityController entityController;
+    private EntityController entityController;
 
-	public GameplayController() {
-		this.entityController = new EntityController();
-	}
-	
-	public void addEntityListener(Listener listener) {
-		entityController.addListener(listener);
-	}
-	
-	public void toggle() {
-		
-	}
+    public GameplayController() {
+        this.entityController = new EntityController();
+    }
+
+    public void addEntityListener(Listener listener) {
+        entityController.addListener(listener);
+    }
+
+    public void toggle() {
+
+    }
+
+    public void removeListeners() {
+        entityController.removeListeners();
+
+    }
 }
