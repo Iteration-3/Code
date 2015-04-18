8f37e7f51f1223a1a99c961f109d87278c13b56b
kkyrazis <kkyrazis@ufl.edu>
2015-04-17 21:04:46 -0400

    Added buttons for save and load slots

--- src/view/layout/OptionsMenuLayout.java
+++ src/view/layout/OptionsMenuLayout.java
@@ -2,6 +2,7 @@ package view.layout;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import controller.OptionsMenuController;
 import view.components.MenuButton;
@@ -12,28 +13,27 @@ public class OptionsMenuLayout extends Layout {
     private MenuButton changeUpKeyButton;
 
     public OptionsMenuLayout() {
-    	setPreferredSize(new Dimension(1024, 768));
-     
+        setPreferredSize(new Dimension(1024, 768));
+        setLayout(new GridLayout(2, 1));
         initButtons();
         addButtons();
     }
-    
+
     private void initButtons() {
         changeUpKeyButton = new MenuButton("REBIND KEYS");
         changeUpKeyButton.setColor(Color.GRAY);
-        
+
         backButton = new MenuButton("GO BACK");
         backButton.setColor(Color.CYAN);
     }
-    
+
     private void addButtons() {
-        add(backButton);
         add(changeUpKeyButton);
+        add(backButton);
     }
 
-    public void attachController(OptionsMenuController controller) {   	
-    	backButton.addActionListener(controller.getBackAction());
-    	changeUpKeyButton.addActionListener(controller.getKeyBindingsAction());
+    public void attachController(OptionsMenuController controller) {
+        backButton.addActionListener(controller.getBackAction());
+        changeUpKeyButton.addActionListener(controller.getKeyBindingsAction());
     }
 }
-
