543aae1674daaf9b4416d1f8df6138ddddb0ea1e
Daniel Gollahon <Daniel.Gollahon@gmail.com>
2015-04-18 23:55:58 -0400

    Adding resizable windows back

--- src/view/View.java
+++ src/view/View.java
@@ -2,9 +2,12 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
@@ -17,18 +20,23 @@ public class View extends JFrame implements ActionListener {
 	private static final int FPS = 30;
 	private static final int REDRAW_INTERVAL = 1000 / FPS;
 	
-	private int i;
+	private int currentLayer;
+	private List<Layout> activeLayouts;
 	
-	private JLayeredPane contentPane;
+	private JLayeredPane layeredPane;
 	
 	public View() {
-		contentPane = getLayeredPane();//new JLayeredPane();
+		layeredPane = getLayeredPane();
 		setPreferredSize(new Dimension(1024, 768));
-		i = 500;
+		currentLayer = 500;
+		
+		activeLayouts = new LinkedList<Layout>();
 		
 		setupFrame();
 
 		initRedrawTimer();
+		
+		addResizeListener();
 	}
 
 	private void setupFrame() {
@@ -49,12 +57,32 @@ public class View extends JFrame implements ActionListener {
 	}
 	
 	public void addGameLayout(Layout layout) {
-		layout.setBounds(new Rectangle(1024, 768));
-		contentPane.add(layout, new Integer(i++));
+		Dimension contentSize = layeredPane.getSize();
+		layout.setSize(getSize());
+		layeredPane.add(layout, new Integer(++currentLayer));
+		activeLayouts.add(layout);
 	}
 	
 	public void removeGameLayout(Layout layout) {
-		contentPane.remove(layout);
-		--i;
+		layeredPane.remove(layout);
+		activeLayouts.remove(layout);
+		--currentLayer;
+	}
+	
+	@Override
+	public void revalidate() {
+		for(Layout activeLayout : activeLayouts) {
+			activeLayout.setBounds(0, 0, getSize().width, getSize().height);
+			activeLayout.revalidate();
+		}
+		super.revalidate();
+	}
+	
+	private void addResizeListener() {
+		this.getRootPane().addComponentListener(new ComponentAdapter() {
+	        public void componentResized(ComponentEvent e) {
+	        	revalidate();
+	        }
+	    });
 	}
 }
