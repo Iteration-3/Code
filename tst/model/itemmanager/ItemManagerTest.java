fa1d37f102c606ef6973be250fe5dfdd83b23f37
kkyrazis <kkyrazis@ufl.edu>
2015-04-18 21:14:02 -0400

    fixed Item Test

--- tst/model/itemmanager/ItemManagerTest.java
+++ tst/model/itemmanager/ItemManagerTest.java
@@ -1,5 +1,9 @@
 package model.itemmanager;
 
+import java.awt.Color;
+
+import model.area.RealCoordinate;
+import model.area.TileCoordinate;
 import model.entity.Smasher;
 import model.item.Boots;
 import model.item.ChestPiece;
@@ -15,34 +19,41 @@ import model.statistics.Statistics;
 import org.junit.Test;
 
 import utilities.structuredmap.StructuredMap;
+import view.EntityView;
 import view.item.BasicItemView;
 
 public class ItemManagerTest {
 
 	@Test
 	public void testItemManagerSaving() {
-		ItemManager manager = new ItemManager(new Smasher());
+		ItemManager manager = new ItemManager(
+				new Smasher("kyle", new EntityView(Color.RED, Color.BLACK,
+						new RealCoordinate(5, 5)), new TileCoordinate(5, 5)));
+		
 		manager.equipToSlot(new Boots(new BasicItemView(), new Statistics()));
-		manager.equipToSlot(new ChestPiece(new BasicItemView(),	new Statistics()));
+		manager.equipToSlot(new ChestPiece(new BasicItemView(),
+				new Statistics()));
+		
 		manager.equipToSlot(new Leggings(new BasicItemView(), new Statistics()));
 		manager.equipToSlot(new Shield(new BasicItemView(), new Statistics()));
-		
-		manager.addItem(new ConsumableItem(new BasicItemView(), new EntityStatistics(), 4.6));
+
+		manager.addItem(new ConsumableItem(new BasicItemView(),
+				new EntityStatistics(), 4.6));
 		manager.addItem(new Gloves(new BasicItemView(), new Statistics()));
 
 		System.out.println(manager.getStructuredMap().getJson());
-		
+
 	}
-	
+
 	@Test
 	public void testInventory() {
 		Inventory inventory = new Inventory();
-		inventory.addItem(new ConsumableItem(new BasicItemView(), new EntityStatistics(), 4.6));
+		inventory.addItem(new ConsumableItem(new BasicItemView(),
+				new EntityStatistics(), 4.6));
 		inventory.addItem(new Gloves(new BasicItemView(), new Statistics()));
-		
-		
+
 		StructuredMap map = inventory.getStructuredMap();
-		
+
 		System.out.println(map.getJson());
 	}
 
