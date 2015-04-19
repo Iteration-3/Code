aae6a5cf97a343d940115b3069d1bc501563fcd2
jackstine <ecstaticjack@gmail.com>
2015-04-19 04:38:17 -0400

    Entity Pursues the Avatar   =)

---
+++ src/model/entity/behavior/npc/defaultb/curcuit/PushableCurcuit.java
@@ -0,0 +1,33 @@
+package model.entity.behavior.npc.defaultb.curcuit;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import utilities.Angle;
+
+public class PushableCurcuit implements Curcuitable{
+	Queue<Angle> moves = new LinkedList<Angle>();
+	
+	public PushableCurcuit(){
+		
+	}
+	
+	public void push(Angle move){
+		this.moves.add(move);
+	}
+
+	public Angle nextMove() {
+		if(moves.isEmpty()){
+			return null;
+		}
+		else{
+			return this.moves.remove();
+		}
+	}
+
+	public boolean hasMove() {
+		return !this.moves.isEmpty();
+	}
+
+
+}
