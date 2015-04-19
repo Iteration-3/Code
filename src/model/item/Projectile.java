9651bd235f6c9e1277f21d53f5b27ac6a0a8cb4c
jackstine <ecstaticjack@gmail.com>
2015-04-19 01:05:35 -0400

    unequipping and equipping items

--- src/model/item/Projectile.java
+++ src/model/item/Projectile.java
@@ -25,11 +25,6 @@ public class Projectile extends EquipableItem{
 	}
 
     @Override
-    public void use(Entity entity) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
     public String getInfo() {
         // TODO Auto-generated method stub
         return null;
