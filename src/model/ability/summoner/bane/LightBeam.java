1624755a8a9baeeb7d721f947e6a530e1a08f46c
joshkirstein <joshkirstein@aol.com>
2015-04-20 00:45:56 -0400

    Hmm

--- src/model/ability/summoner/bane/LightBeam.java
+++ src/model/ability/summoner/bane/LightBeam.java
@@ -2,14 +2,13 @@ package model.ability.summoner.bane;
 
 import model.ability.ProjectileAbility;
 import model.area.GrowingConicalArea;
-import model.area.RadialArea;
+import model.area.GrowingRadialArea;
 import model.entity.Avatar;
 import model.entity.Entity;
 import model.event.Event;
 import model.event.HealthModifierEvent;
 import model.projectile.conical.LightConeProjectile;
 import model.skillmanager.SummonerSkillManager;
-import model.trigger.SingleUseTrigger;
 
 public final class LightBeam extends ProjectileAbility {
 	
@@ -26,10 +25,9 @@ public final class LightBeam extends ProjectileAbility {
 	
 	@Override
 	public LightConeProjectile getProjectile(Entity ent){
-		System.out.println("ONCE");
 		Event damageEvent = new HealthModifierEvent(ent, null, 0, -15*manager.getBaneSkill());
 
-		return new LightConeProjectile(ent.getLocation(), ent.getDirection(), new GrowingConicalArea(ent.getLocation(), ent.getDirection(), 10), damageEvent, 2.2);
+		return new LightConeProjectile(ent.getLocation(), ent.getDirection(), new GrowingRadialArea(ent.getLocation(), ent.getDirection(), 10), damageEvent, 2.2);
 	}
 
 	@Override
