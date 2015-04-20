54b2a0bab596fabbf9fa764b313f78dce08c230a
jackstine <ecstaticjack@gmail.com>
2015-04-20 00:18:17 -0400

    the NPC attacks you

--- src/model/entity/behavior/npc/interact/AttackInteract.java
+++ src/model/entity/behavior/npc/interact/AttackInteract.java
@@ -3,15 +3,26 @@ package model.entity.behavior.npc.interact;
 import model.entity.Avatar;
 import model.entity.Entity;
 import model.entity.NPC;
+import model.event.EventManager;
+import model.event.HealthModifierEvent;
 
 public class AttackInteract implements InteractableBehaviorState {
+	private Entity chosen;
+	
+	public AttackInteract(Entity entity){
+		this.chosen = entity;
+	}
 
 	public void accept(NPC entity) {
 
 	}
 
 	public void accept(Avatar avatar) {
-		
+		//TODO  damage needs to be modified with skill points
+		int damage = this.chosen.getDamage() * -1;
+		EventManager.getSingleton().addEvent(new HealthModifierEvent(this.chosen,avatar,0,damage));
+		this.chosen.setAttacking();
+		avatar.setHasBeenAttacked();
 	}
 
 	public void interact(Entity entity) {
